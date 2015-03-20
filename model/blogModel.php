f60418e449ce340bd618b95998428daff1b9b5f9
dudekm <mateusz.dudek.zg@gmail.com>
2015-03-20 20:10:14 +0100

    update

--- model/blogModel.php
+++ model/blogModel.php
@@ -1,13 +1,10 @@
 <?php
 //blogModel.php
 class blogModel {
-    //tablica do ktorej dodawane sa posty z Repo przy pomocy funkcji modelAddPost
-    private $posts = [];
-    
+ 
     public function __construct() {
         $db = new PDO('mysql:host=localhost;dbname=blog', 'root', '');
-    }
-    
+    }    
     //zwrocenie wyniku wszystkich postow do kontrolera
     public function findAll() {
         $query = $db->query('SELECT * FROM posts');
