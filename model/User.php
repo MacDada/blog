43cb8fc3104576225c3c64d6cabc4f0583fcd4cb
dudekm <mateusz.dudek.zg@gmail.com>
2015-03-23 21:24:07 +0100

    update

---
+++ model/User.php
@@ -0,0 +1,30 @@
+<?php
+class User {
+    private $id;
+    private $username;
+    private $password;
+    private $email;
+    
+    public function __construct($id, $username, $password, $email) {
+        $this->id = $id;
+        $this->login = $username;
+        $this->password = $password;
+        $this->email = $email;
+    }
+    
+    public function getID($id) {
+        $this->id;
+    }
+    
+    public function getLogin($username) {
+        $this->username;
+    }
+    
+    public function getPassword($password) {
+        $this->password;
+    }
+    
+    public function getEmail($email) {
+        $this->email;
+    }
+}
